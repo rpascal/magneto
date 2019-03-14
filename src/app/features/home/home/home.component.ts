be50505c8aedf00d24d509be1631e7cb44653b5a
rpascal-Laptop <ryanpascal99@yahoo.com>
2019-03-14 13:20:09 -0400

    Adding bootstrap

--- src/app/features/home/home/home.component.ts
+++ src/app/features/home/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ProductDataService, Topic } from 'src/app/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+
 
 @Component({
   selector: 'cbc-home',
@@ -8,11 +11,26 @@ import { ProductDataService, Topic } from 'src/app/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-
   data$: Observable<Topic>;
 
-  constructor(private productDataService: ProductDataService) { }
+  itemSize = 250;
+
+  @ViewChild('scrollArea') scrollArea: CdkVirtualScrollViewport;
+
+
+  constructor(private productDataService: ProductDataService, breakpointObserver: BreakpointObserver) {
+    breakpointObserver.observe([
+      Breakpoints.XSmall,
+    ]).subscribe(result => {
+      console.log(result);
+      if (result.matches) {
+        this.itemSize = 500;
+      } else {
+        this.itemSize = 250;
+      }
+      this.scrollArea.checkViewportSize();
+    });
+  }
 
   ngOnInit() {
     this.data$ = this.productDataService.getData('./assets/data/defaultData.json');
